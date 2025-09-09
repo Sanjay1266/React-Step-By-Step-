48e43a8c34929168984a667b624abeefde98c460
Sanjay1266 <sanjaysundaram50@gmail.com>
2025-09-09 23:59:09 +0530

    The introduction for the react is completed along with telling what is the component

--- React-project/src/Food.jsx
+++ React-project/src/Food.jsx
@@ -5,6 +5,8 @@ function Food(){
         <ul>
             <li>This is the apple for the Bananna</li>
             <li>{food1}</li>
+            <li>{food2. toLocaleLowerCase()}</li>
+            
         </ul>
     );
 
